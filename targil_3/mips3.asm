b714ad723d6685c1c73ad6722c2460f55733f401
dattali18 <74492594+dattali18@users.noreply.github.com>
2022-03-14 13:21:31 +0200

    Update mips3.asm

--- targil_3/mips3.asm
+++ targil_3/mips3.asm
@@ -1,30 +1,32 @@
 .text
 Main:
-	j Ex_3.2		# choosing wich program to run
+	j Ex_3.2		# choosing which program to run
 	
 	
 Ex_3.1:
-	li $a0, 0		# a0: is the a0 of an arithmetic serie
-	li $a1, 2		# a1: is the d in the serie (difference)
+	li $a0, 0		# a0: is the a0 of an arithmetic series
+	li $a1, 2		# a1: is the d in the series (difference)
 	li $v0, 0		# v0: is used as the container of the summation
 	li $t0, 11		# t0: is used as the counter (or i in a for loop)
 				# to sum 10 element the counter need to start at 11
 Loop_1:
 	add $v0, $v0, $a0	# adding the current element a(n) to the sum 
-	add $a0, $a0, $a1	# moving a0 to the next element in the serie a(n+1)
+	add $a0, $a0, $a1	# moving a0 to the next element in the series a(n+1)
 	addi $t0, $t0, -1	# decrementing the counter by one
 	bne $t0, $zero, Loop_1	# while( t0 != 0) jump back to the start of the loop
 	j Exit
 	
+	# in the case of X being 0 we evaluate to be 1 like in calc 2
+	# in the case of n being 0 we evaluate to be 0
 	
-	# for the information I have assume that if n = 0 the sum should be 1
 Ex_3.2:
-	li $a0, 6		# a0 : is the number of elements (n) in the serie
-	li $a1, 5		# a1 : is the var X
-	li $v0, 1		# v0 : is the container for the summation
+	li $a0, 6		# a0 : is the number of elements (n) in the series
+	li $a1, 0		# a1 : is the var X
+	li $v0, 1		# v0 : is the container for the summation starting from 1 because the first element is always 1
 	li $t0, 1		# t0 : is used as the container to calculate  X^n
 	add $t1, $zero, $a0	# t1 : is used as the first counter i
 	addi $t2, $t1, -1	# t2 : is used as the second counter j 
+	beq $a0, $zero, case_0	# if a0 (n) = 0 so the result is 0
 Loop_2.0:
 	j Loop_2.1
 Continue:
@@ -44,18 +46,9 @@ Loop_2.1:
 	bne $t2, $zero, Loop_2.1# while( t2 !=) jump back to the start of the inner loop
 	j Continue		# jumping back to the outer loop
 
-
+case_0:
+	add $v0, $zero,$zero	# if n = 0 so the sum = 0
+	j Exit
 
 Exit:
 	nop
-
-# int sum = 0;
-# int x = 5;
-# int product = 1;
-# for(int i = 1; i <= n; i++) {
-#	for(int j = 0; j < i - 1; j++) {
-#		product *= x;
-#	}
-#	sum += product * i;
-#	product = 1;
-# }
